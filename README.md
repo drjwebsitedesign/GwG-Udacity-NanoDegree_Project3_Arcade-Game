f8b2b35a81704e29473db30543ca3065d62d48b4
JEROME-WILSON@DR_J_WEB_DESIGN <JEROME-WILSON@DR_J_WEB_DESIGN>
2018-09-28 19:39:29 -0400

    removed errors from app.js using jsLint. also updated readme.

--- README.md
+++ README.md
@@ -1,29 +1,42 @@
 frontend-nanodegree-arcade-game
 ===============================
-SOME CHANGES I MADE TO THE GAME ARE:
 
-1.Make The Game 95% Hands-Free!
+###Project-4 The rubric for the Grow-with-Google Udacity Nanodegree Scholarship.
+Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** is updated with your instructions on both how to 1. Run and 2. Play your arcade game. 
+
+Click here to see a 3-min youtube video of the [Arcade Game](https://www.youtube.com/watch?v=dIu40myH9CY) uploaded by Esther Kim.
+- - - - - - - - - -
+###The Technologies used to build this game.
+This game was built with pure vanilla html, css, and es6 javascript.
+- - - - - - - - - -
+###How-to Play this Arcade Clone frogger Game.
+waterGirl only has '5 Lives' to make it home safely! waterGirl's loving aquatic family lives at the bottom of the world in the Pacific ocean. Her aquatic family can only survive on dry land for a short time. 
+
+waterGirl is headed home from visiting her best friend at the top of the world in the Atlantic ocean. They both fear the gigantic landBugs that eat the waterPeople to survive! 
+
+Can waterGirl return home without being eaten by the landBugs?? Can she safely cross the hard gravel roads and thick grassy weeds?? If she fails it will be ..... Rest in Peace for waterGirl!
+
+#####The Challenges for the Players against the landBugs.
+waterGirl has 5 lives to make it safely home from the top of the world(Atlantic Ocean) back to the bottom of the world(Pacific Ocean). she loses a life 'each time' she collides with a landBug. She gets eaten by the landBug if she collides 5 times. 
+
+However, if waterGirl makes it all the way to the Pacific Ocean without colliding with a landBug, then the player (You) wins the game. The player will receive 3 different messages to update your progress. The messages are: 1)You lose a life, 2)you lose the game, or 3)you win.
+- - - - - - - - - -
+###Some Creative Changes i made to make the game more Challenging are:
+*Make The Game 95% Hands-Free!
 The main creative focus driving my version of this game was to make it 95% hands-free for the players. That allows the players to focus 95% of their hand movements on using the 'arrow keys' to "safely navigate waterGirl", instead of deleting popUps, modals, 'X's, or buttons, etc. If the players have to constantly stop to remove 'event messages' displayed during the game, then the player has to mentally and emotionally re-focus their energy each time they move their hand from the 'arrow keys', over to the 'mouse', and then back over to the 'arrow keys'. 
 
 This distraction has been eliminated by automating this game's four 'event messages' which are: a)the player loses a Life, b)the player loses the game after losing 5 lives, c)the player wins the game if waterGirl safely and successfully makes it home from the 'top of the world' to the 'bottom of the world' without being eaten by the 'landBugs', and finally lol, d)the 'rules of the game and the theme of the game' button. I used the 'setTimeout method' to automate the opening and closing of each 'event message' so that the players can keep their fingers positioned on the 'arrow keys' tne entire game. The automation also allows them to focus 100% of their energy ONLY on winning the game. 
 
-2.CREATE A 'GAME THEME' THAT MAKES THE PLAYERS COMPASSIONATE TO wATERGIRL'S SAFETY!
-        'click here and read the game rules'
-waterGirl only has '5 Lives' to make it home safely! The loving aquatic family of waterGirl lives at the bottom of the world in the Pacific ocean. Her aquatic family can only survive on dry land for a short time. waterGirl is headed home from visiting her best friend at the top of the world in the Atlantic ocean. They fear the gigantic landBugs that eat the waterPeople! Can she safely cross the hard gravel roads and thick grassy weeds ?? Can waterGirl return home without being eaten by the landBugs ? If she fails it will be ..... Rest in Peace for waterGirl!
+*Create a Game Theme that makes the players compassionate waterGirl's safety!
     
-3.CREATED A DIFFERENT 'SCENERY' TO MAKE THE GAME EVEN MORE CHALLENGING THAN BEFORE!
-a.Added a 4th landBug to evade to make it more difficult for waterGirl while returning home.
+*Created a different Scenery to make the game even more challenging than before! 
 
-b.Display a countdown of the number of lives waterGirl has left after being eaten by each landBug.
+*Added a 4th landBug to evade to make it more difficult for waterGirl while returning home.
 
-c.waterGirl's journey is reversed. She travels from the top of the canvas to the bottom, instead of from the bottom to the top. Her 'y' coordinates are changed to make the code work.
+*Display a countdown of the number of lives waterGirl has left after being eaten by each landBug.
 
-d.I re-arranged the rowImages to fit my 'game theme' and to create a more challenging trek across the roads, water, and grasslands. Re-arranged from 1-water, 3-stone, 2-grass ..... to 1-water(Atlantic Ocean), 2-stone(landBug turf), 2-grass(landBug turf), and 1-more water(Pacific Ocean).
+*waterGirl's journey is reversed. She travels from the top of the canvas to the bottom, instead of from the bottom to the top. Her 'y' coordinates are changed to make the code work.
 
-e.Reduced the canvas.height from 606 to 515 to help the canvas fit more squarely and centered on the body. The canvas would briefly overlap the page whenever the height of my '<h2> display messages' would render. 
+*I re-arranged the rowImages to fit my 'game theme' and to create a more challenging trek across the roads, water, and grasslands. Re-arranged from 1-water, 3-stone, 2-grass ..... to 1-water(Atlantic Ocean), 2-stone(landBug turf), 2-grass(landBug turf), and 1-more water(Pacific Ocean).
 
-
-
-3.TECHNOLOGIES AND DEPENDANCIES USED:
-a.html, css, javascript
-b.Changed the code from ES5(.prototype) to ES6(Classes).
+*Reduced the canvas.height from 606 to 515 to help the canvas fit more squarely and centered on the body. The canvas would briefly overlap the page whenever the height of my display messages would render.
--- js/app.js
+++ js/app.js
@@ -2,25 +2,24 @@
 
 let numLives = 5;
 
-// THE PLAYER CAN CLICK THE GAME THEME BUTTON TO DISPLAY ITS HIDDEN TEXT AND READ THE GAME'S THEME AND RULES. THE TEXT WILL BE DYNAMICALLY HIDDEN AGAIN AFTER 14 SECONDS(SETTIMEOUT).
+//CLICK THE GAME THEME BUTTON TO DISPLAY THE HIDDEN GAME THEME AND RULES. TEXT DYNAMICALLY HIDES AGAIN AFTER 14 SECONDS.
 function GameThemeBtn(){     
     document.getElementById("gameTheme").style.display = "contents";    
         setTimeout (function() {
             document.getElementById("gameTheme").style.display = "none";
             },14000);            
-}; 
+}
 
 
 
 
-//THE ENEMY IN THIS GAME THEME ARE THE LANDBUGS.
-//THE METHODS NESTED INSIDE THIS "ENEMY" CLASS ARE: THE CONSTRUCTOR, THE TIME DELTA UPDATES, THE COLLISION DETECTION, AND THE CANVAS RENDERING,
+//THE METHODS NESTED INSIDE THIS "ENEMY" CLASS ARE: CONSTRUCTOR, TIME DELTA UPDATES, COLLISION DETECTION, AND CANVAS RENDERING.
 class Enemy {
     constructor(x, y, speed){
         this.x=x;
         this.y=y;
         this.speed=speed;
-        this.sprite='images/enemy-bug.png';
+        this.sprite="images/enemy-bug.png";
     }
     
     update(dt){
@@ -43,35 +42,34 @@ class Enemy {
     render(){
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-};
+}
 
 
 
 
 
-//THE PLAYER IN THIS GAME THEME IS WATERGIRL.
-//THE METHODS NESTED INSIDE THIS "PLAYER" CLASS ARE: THE CONSTRUCTOR, THE DELTA TIME UPDATES, THE CANVAS RENDERING, COLLISION DETECTION, THE KEY CONTROLS, THE "WIN GAME/RESET" FUNCTIONS. 
+//THE METHODS NESTED INSIDE THIS CLASS ARE: CONSTRUCTOR, DELTA TIME UPDATES, CANVAS RENDERING, COLLISION DETECTION, KEY CONTROLS, "WIN GAME/RESET" FUNCTIONS. 
 class Player {
     constructor(x, y) {
         this.x = x;
         this.y = y;    
-        this.sprite = 'images/char-water-girl.png';
+        this.sprite = "images/char-water-girl.png";
     }    
         update() {            
         }
         render() {ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
         }    
         handleInput(key) {    
-            if (key == 'left' && this.x > 10) {
+            if (key == "left" && this.x > 10) {
               this.x -= 101;
             }
-            if (key == 'right' && this.x < 405) {
+            if (key == "right" && this.x < 405) {
               this.x += 101;
             }
-            if (key == 'up' && this.y > 0) {
+            if (key == "up" && this.y > 0) {
               this.y -= 83;
             }
-            if (key == 'down' && this.y < 400) {
+            if (key == "down" && this.y < 400) {
               this.y += 83;
             }
 
@@ -81,7 +79,7 @@ class Player {
                 resetGame();              
             }
       }    
-};
+}
 
 
 
@@ -92,13 +90,13 @@ const allEnemies = [new Enemy(0, 63, 200), new Enemy(0, 145, 60), new Enemy(0, 2
 // Place the player object in a variable called player
 const player = new Player(203, 0);
 
-// This listens for key presses and sends the keys to your Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+// This listens for key presses and sends the keys to your Player.handleInput() method.
+document.addEventListener("keyup", function(e) {
+    let allowedKeys = {
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
     };    player.handleInput(allowedKeys[e.keyCode]);
 });
 
@@ -114,31 +112,31 @@ function loseLife(){
   }else if(numLives === 1){
         loseGame();
         }
-};
-// RESET THE LOCATION OF THE PLAYER TO THE 'START GAME' LOCATION, BUT, DO NOT RESET THE NUMBER OF LIVES LOST. CONTINUE PLAYING THE GAME UNTIL THE PLAYER EITHER WINS OR LOSES ALL 5 LIVES.
+}
+// RESET PLAYER TO THE 'START GAME' LOCATION, BUT, DO NOT RESET THE NUMBER OF LIVES LOST. CONTINUE PLAYING GAME UNTIL THE PLAYER EITHER WINS OR SHE LOSES ALL 5 LIVES.
 function resetPlayer(){
     player.x = 203;
     player.y = 0;
-};
-// START A 'NEW GAME'. RESET THE LOCATION OF THE PLAYER TO THE 'NEW GAME' LOCATION. ALSO, RESET NUMLIVES FROM 0 LIVES BACK TO 5 LIVES. AND RENDER THE TITLE OF THE GAME AGAIN.
+}
+// START 'NEW GAME'. RESET PLAYER TO 'NEW GAME' LOCATION. ALSO, RESET NUMLIVES FROM 0 LIVES BACK TO 5 LIVES. RENDER THE TITLE OF THE GAME AGAIN.
 function resetGame(){
     resetPlayer();
     numLives = 5; 
     gameTitle();
-};
+}
 
 
 
 
 // BELOW ARE 4 CONDITIONS: 'LOSE A LIFE', 'LOSE A GAME', 'WIN A GAME, AND HIDE/SHOW THE GAME TITLE !!!'. THE PLAYER WILL RECEIVE A MESSAGE, DYNAMICALLY-GENERATED, FOR EACH CONDITION. THE IDEA IS TO MAKE THE GAME AS FAST-PACED, "HANDS-FREE", AND ENJOYABLE AS POSSIBLE FOR THE PLAYERS. I ACCOMPLISH THIS BY USING THE 'SET TIMEOUT METHOD' TO DYNAMICALLY HIDE/SHOW EACH INNERHTML MESSAGE. THIS WAY, THE PLAYER DOES NOT HAVE TO STOP PLAYING TO "CLICK & CLOSE" SO MANY 'MESSAGE-BEARING' MODALS. ALSO, THE PLAYER DOES NOT HAVE TO MAKE NUMEROUS EXTRA 'HAND MOVEMENTS' (FROM THE KEYBOARD TO THE MOUSE AND THEN BACK TO THE KEYBOARD)! LESS WORK TIME ... MORE PLAY TIME.
 
-//DYNAMICALLY HIDE THE GAME TITLE DURING THE GAME TO REMOVE DISTRACTIONS. SHOW TITLE AGAIN AT THE BEGINNING OF EACH NEW GAME.
+//DYNAMICALLY HIDE THE GAME TITLE DURING THE GAME TO REMOVE DISTRACTIONS. SHOW TITLE AGAIN AT BEGINNING OF EACH NEW GAME.
 function gameTitle(){
     document.getElementById("gameTitle").innerHTML = ""; 
         setTimeout(function(){
             document.getElementById("gameTitle").innerHTML = "Please help waterGirl make it home safely!"; 
         },1500);
-};
+}
 
 // DYNAMICALLY SEND MESSAGE TO THE PLAYER EACH TIME SHE LOSE A LIFE WHEN COLLIDING WITH A LANDBUG. ALSO COUNTDOWN HOW MANY LIVES SHE HAS LEFT BEFORE THE GAME IS OVER.
 function loseLifeMsg(){    
@@ -147,7 +145,7 @@ function loseLifeMsg(){
         document.getElementById("gameTitle").innerHTML = "";            
         },750);
                     resetPlayer();
-};
+}
 
 // DYNAMICALLY SEND THE PLAYER A MESSAGE EACH TIME SHE LOSE A GAME. THEN RESET THE GAME.
 function loseGame(){     
@@ -156,7 +154,7 @@ function loseGame(){
         document.getElementById("loseGameMsg").style.display = "none";            
         },1200);
                     resetGame();
-};
+}
 
 // DYNAMICALLY SEND THE PLAYER A MESSAGE EACH TIME SHE WIN A GAME. THEN RESET THE GAME.
 function winGame(){
@@ -165,4 +163,4 @@ function winGame(){
         document.getElementById("winGameMsg").style.display="none";
         },1200);  
                     resetGame();
-};
+}
